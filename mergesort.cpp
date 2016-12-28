818d2b65d4d2c9363a6fbd5049ffb56a8a2ce57d
Daniel Tsai <idaniel.twc@gmail.com>
2016-12-29 00:43:11 +0800

    init commit

---
+++ mergesort.cpp
@@ -0,0 +1,67 @@
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+template <class arr_T>
+void Merge(arr_T& array, arr_T& result, int left, int mid, int right)
+{
+    int l = left;
+    int r = mid;
+    int k = left;
+    while((l <= mid-1) && (r <= right)){
+        if(array[l]<=array[r]){
+            result[k++] = array[l++];
+        }else{
+            result[k++] = array[r++];
+        }
+    }
+    while(l <= mid-1){
+        result[k++] = array[l++];
+    }
+    while(r <= right){
+        result[k++] = array[r++];
+    }
+    // write back for next time use
+    int i;
+    for(i=left;i<=right;i++){
+        array[i] = result[i];
+    }
+}
+
+template <class arr_T>
+void MergeSort(arr_T& array, arr_T& result, int left, int right)
+{
+    if(left < right){
+        int mid = (left+right)/2;
+        MergeSort(array,result,left,mid);
+        MergeSort(array,result,mid+1,right);
+        Merge(array,result,left,mid+1,right);
+    }
+}
+
+int main()
+{
+    fstream fin;
+    fin.open("sort.txt",ios::in);
+    string line;
+    vector<int> unsort;
+    while(getline(fin,line)){
+        static stringstream ss(line);
+        static int tmp;
+        while(ss >> tmp){
+            unsort.push_back(tmp);
+        }
+    }
+    vector<int> result;
+    result.resize(unsort.size());
+
+    MergeSort(unsort, result,0,unsort.size()-1);
+    for(uint32_t i = 0; i < result.size(); i++) {
+        cout<<result[i]<<" ";
+    }
+    return 0;
+}
